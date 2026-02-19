99c1512f3d9a74ec1f4033b196e6bea34bb1b2f0
tk1351 <takakikitsui0111@gmail.com>
2026-02-19 12:42:07 +0900

    feat: header

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,13 +5,26 @@ import {
   IonTitle,
   IonContent,
 } from '@ionic/angular/standalone';
+import { IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { personCircleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon],
 })
 export class HomePage {
-  constructor() {}
+  private today = new Date();
+  isoDate = this.today.toISOString().split('T')[0];
+  formattedDate = new Intl.DateTimeFormat(navigator.language, {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  }).format(this.today);
+
+  constructor() {
+    addIcons({ personCircleOutline });
+  }
 }
